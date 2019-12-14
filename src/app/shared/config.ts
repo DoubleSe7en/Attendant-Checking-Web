46a8230acdd057613c060ab9794b20a4437af0b6
Nguyen Cuong <nguyencuong1827@gmail.com>
2019-12-14 22:12:55 +0700

    fix apiConfig

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-    public apiHost = "https://tpltesting.herokuapp.com/api";
-    public host = "https://tpltesting.herokuapp.com";
+    public apiHost = "https://attendant-checking-for-web.com/api";
+    public host = "https://attendant-checking-for-web.herokuapp.com";
 }
